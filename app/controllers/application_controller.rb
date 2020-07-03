facbd0d5ac11a0fa7a0235055ab3a7293a6d1b54
Alejandro Raffo <amsterdamAL@gmail.com>
2020-07-03 12:36:16 -0400

    heroku error fix, skip token in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery unless: -> { request.format.json? }
-    skip_before_action :verify_authenticity_token
+    #protect_from_forgery unless: -> { request.format.json? }
+    skip_before_action :verify_authenticity_token , raise: false
 end
 
 
