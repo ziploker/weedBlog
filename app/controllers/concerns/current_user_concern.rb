56fe866b7dfe42e91afd23df4a4cc85171142ee7
Alejandro Raffo <amsterdamAL@gmail.com>
2020-07-03 15:10:26 -0400

    debugging, in production server returning no user is logged in

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,8 +6,12 @@ module CurrentUserConcern
     end
 
     def set_current_user
+        puts 'in set current user =============================='
         if session[:user_id]
+            puts 'in session[:user_id] =============================='
+            puts 'session[:user_id] is ' + session[:user_id].to_s
             @current_user = User.find(session[:user_id])
+            puts '@current_user = ' + @current_user.inspect
         end
     end
 end
