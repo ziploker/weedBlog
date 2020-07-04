04c0c1242ce40ce86d6237d546a45231945cfadd
Alejandro Raffo <amsterdamAL@gmail.com>
2020-07-04 08:50:58 -0400

    debugging, in production server returning no user is logged in

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -7,6 +7,14 @@ module CurrentUserConcern
 
     def set_current_user
         puts 'in set current user =============================='
+        puts 'MODE = ' + Rails.env
+
+        if !session[:x]
+            session[:x] = 0
+        end
+
+        session[:x] += 1
+        puts 'session[:x] is currently equal to = ' + session[:x].to_s
         if session["user_id"]
             puts 'in session[:user_id] =============================='
             puts 'session[:user_id] is ' + session[:user_id].to_s
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_autentication_app", secure: true, domain: :all
-else
+if Rails.env == "development"
+
+    
     Rails.application.config.session_store :cookie_store, key: "_autentication_app"
+else
+    Rails.application.config.session_store :cookie_store, key: "_autentication_app", secure: true, domain: :all
 end
