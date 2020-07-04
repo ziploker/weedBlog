0550b1a5b53482fe3f9033720835f2f6b11f99a8
Alejandro Raffo <amsterdamAL@gmail.com>
2020-07-04 11:28:21 -0400

    change session cookies to expire after 60min, instead of on browser close

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == "development"
 
     
-    Rails.application.config.session_store :cookie_store, key: "_autentication_app"
+    Rails.application.config.session_store :cookie_store, key: "_autentication_app", :expire_after => 60.minutes
 else
-    Rails.application.config.session_store :cookie_store, key: "_autentication_app"
+    Rails.application.config.session_store :cookie_store, key: "_autentication_app", :expire_after => 60.minutes
 end
