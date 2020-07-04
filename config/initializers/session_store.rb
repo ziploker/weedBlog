619bb829165fb521bf3f3c10d20ef2714ad37b81
Alejandro Raffo <amsterdamAL@gmail.com>
2020-07-04 01:55:57 -0400

    debugging, in production server returning no user is logged in

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_autentication_app", domain: :all
+    Rails.application.config.session_store :cookie_store, key: "_autentication_app", secure: true, domain: :all
 else
     Rails.application.config.session_store :cookie_store, key: "_autentication_app"
 end
